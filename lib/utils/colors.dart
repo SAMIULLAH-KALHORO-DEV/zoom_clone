53526c04ebc735d0d10e41ac85c1090d8927a508
SAMIULLAH-KALHORO-DEV <samiullahabbasy@gmail.com>
2023-07-24 09:27:51 +0500

    some new colors added

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -4,3 +4,4 @@ const backgroundColor = Color.fromRGBO(36, 36, 36, 1);
 const footerColor = Color.fromRGBO(14, 114, 236, 1);
 const buttonColor = Color.fromRGBO(26, 26, 26, 1);
 const secondaryBackgroundColor = Color.fromRGBO(46, 46, 46, 1);
+const secondaryBackgroundColors = Color.fromRGBO(46, 46, 46, 1);
