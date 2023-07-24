42998048494e07ee48f35eecfbf0ab488da0969b
SAMIULLAH-KALHORO-DEV <samiullahabbasy@gmail.com>
2023-07-24 10:22:40 +0500

    day 2 working on it

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:zoom_clone/firebase_options.dart';
+import 'package:zoom_clone/resources/auth_methods.dart';
 import 'package:zoom_clone/screens/home_screen.dart';
 import 'package:zoom_clone/screens/login_screen.dart';
 import 'package:zoom_clone/utils/colors.dart';
@@ -19,14 +20,29 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: "Zoom Clone",
-        theme:
-            ThemeData.dark().copyWith(scaffoldBackgroundColor: backgroundColor),
-        routes: {
-          '/login': (context) => const LoginScreen(),
-          '/home': (context) => const HomeScreen()
+      debugShowCheckedModeBanner: false,
+      title: "Zoom Clone",
+      theme:
+          ThemeData.dark().copyWith(scaffoldBackgroundColor: backgroundColor),
+      routes: {
+        '/login': (context) => const LoginScreen(),
+        '/home': (context) => const HomeScreen()
+      },
+      home: StreamBuilder(
+        stream: AutoMethods().authChanges,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (snapshot.hasData) {
+            return const HomeScreen();
+          }
+
+          return HomeScreen();
         },
-        home: const LoginScreen());
+      ),
+    );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:zoom_clone/utils/colors.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,23 +9,41 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _page = 0;
+  onPageChanged(int page) {
+    setState(() {
+      _page = page;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [
-        Expanded(
-          flex: 2,
-          child: Container(color: Colors.red),
-        ),
-        Expanded(
-          flex: 1,
-          child: Container(color: Colors.blue),
-        ),
-        Expanded(
-          flex: 1,
-          child: Container(color: Colors.green),
-        ),
-      ]),
+      appBar: AppBar(
+        elevation: 0.0,
+        title: const Text("Meet & Chat "),
+        centerTitle: true,
+        backgroundColor: backgroundColor,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.grey,
+          onTap: onPageChanged,
+          currentIndex: _page,
+          unselectedFontSize: 14,
+          type: BottomNavigationBarType.fixed,
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.comment_bank), label: "Meet & Chat"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.lock_clock), label: "Meetings"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.person_2_outlined), label: "Contacts "),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.settings_outlined), label: "Meet & Chat"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.comment_bank), label: "Meet & Chat"),
+          ]),
     );
   }
 }
