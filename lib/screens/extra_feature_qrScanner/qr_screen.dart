17aaba307354b598c2f488686559b2fe01bd157c
SAMIULLAH-KALHORO-DEV <samiullahabbasy@gmail.com>
2023-08-02 14:20:59 +0500

    just added extra feature not impelement or initiazed it implement if needed

---
+++ lib/screens/extra_feature_qrScanner/qr_screen.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:zoom_clone/screens/extra_feature_qrScanner/result_screen.dart';
+// import 'package:mobile_scanner/mobile_scanner.dart';
+// import 'package:qr_scanner/result_screen.dart';
+
+const bgColor = Colors.white;
+
+class QRscreen extends StatefulWidget {
+  const QRscreen({super.key});
+
+  @override
+  State<QRscreen> createState() => _QRscreenState();
+}
+
+class _QRscreenState extends State<QRscreen> {
+  bool isScanCompleted = false;
+  void closeScreen() {
+    isScanCompleted = false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: bgColor,
+      appBar: AppBar(
+        title: const Text("QR Scanner",
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.black87,
+            )),
+        centerTitle: true,
+      ),
+      body: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.all(16),
+        child: Column(children: [
+          Expanded(
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                Text(
+              "Place the QR code in that aera ",
+              style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black87,
+                  letterSpacing: 1),
+                ),
+                SizedBox(height: 10),
+                Text("Scanning will be started automatically",
+                style: TextStyle(
+                    fontSize: 16, color: Colors.black54, letterSpacing: 1)),
+              ])),
+          // Expanded(
+          //   flex: 4,
+          //   child: MobileScanner(
+          //     onDetect: (barcodes) {
+          //       if (!isScanCompleted) {
+          //         String code = barcodes.raw ?? '---';
+          //         isScanCompleted = true;
+          //         Navigator.push(
+          //             context,
+          //             MaterialPageRoute(
+          //               builder: (context) =>
+          //                   ResultScreen(closeScreen: closeScreen, code: code),
+          //             ));
+          //       }
+          //     },
+          //   ),
+          // ),
+          Expanded(
+              child: Container(
+            color: Colors.green,
+          )),
+        ]),
+      ),
+    );
+  }
+}
---
+++ lib/screens/extra_feature_qrScanner/result_screen.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+// import 'package:qr_flutter/qr_flutter.dart';
+
+class ResultScreen extends StatelessWidget {
+  final String code;
+  final Function() closeScreen;
+  const ResultScreen(
+      {super.key, required this.closeScreen, required this.code});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("QR Scanner",
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.black87,
+            )),
+        centerTitle: true,
+        leading: IconButton(
+          onPressed: () {
+            closeScreen();
+            Navigator.pop(context);
+          },
+          icon: const Icon(
+            Icons.arrow_back_ios_new_outlined,
+            color: Colors.black87,
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          children: [
+            // show QR code here
+            // QrImage(
+            //   data: code,
+            //   size: 150,
+            //   version: QrVersions.auto,
+            // ),
+            const Text("Scanned Result",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black54,
+                )),
+            const SizedBox(height: 10),
+            Text(
+              code,
+              style: const TextStyle(
+                fontSize: 16,
+                color: Colors.black87,
+              ),
+            ),
+            const SizedBox(height: 10),
+            SizedBox(
+              width: MediaQuery.of(context).size.width - 100,
+              height: 48,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green.shade300),
+                onPressed: () {},
+                child: const Text(
+                  "Copy",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black87,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
